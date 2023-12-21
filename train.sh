05f1f34b24c5c50cc972e4db70d219509dcec0f1
chestnutlzj <2533221180@qq.com>
2023-12-21 19:33:25 +0800

    update train.sh

--- train.sh
+++ train.sh
@@ -6,18 +6,17 @@ accelerate launch train.py \
   --dataset_name=$DATASET_NAME \
   --enable_xformers_memory_efficient_attention \
   --resolution=1024 \
-  --proportion_empty_prompts=0.2 \
   --train_batch_size=4 \
   --gradient_accumulation_steps=4 \
-  --max_train_steps=500 \
+  --max_train_steps=3000 \
   --use_8bit_adam \
-  --learning_rate=1e-05 \
+  --learning_rate=1e-06 \
   --lr_scheduler="cosine" \
   --lr_warmup_steps=0 \
   --mixed_precision="fp16" \
   --report_to="wandb" \
   --checkpointing_steps=50 \
-  --output_dir="sdxl-pokemon-model" \
+  --output_dir="ai-st-dpo-model" \
   --good_image_column="good_jpg" \
   --bad_image_column="bad_jpg" \
   --caption_column="caption" \
